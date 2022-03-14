5ea8c56375449e713f4b62520d500a8e6452b290
bagas ardianto <bagasardiantobks2020@gmail.com>
2022-03-14 19:25:36 +0700

    mencetak literal boolean

---
+++ src/N_LiterealBoelean.java
@@ -0,0 +1,8 @@
+public class N_LiterealBoelean {
+    public static void main(String[] args) {
+        boolean isopen=true;
+        boolean isclose=false;
+        System.out.println("contoh litereal boelean kondisi" + isopen);
+        System.out.println("contoh litereal boelean kondisi" + isclose);
+    }
+}
