7390d1849c5eb51d3e4de6ec46b567444f9bb283
bagas ardianto <bagasardiantobks2020@gmail.com>
2022-03-14 17:18:16 +0700

    mencetak nilai character

---
+++ src/K_LiteralChar.java
@@ -0,0 +1,13 @@
+public interface K_LiteralChar {
+    public static void main(String[] args) {
+        char tab='\t';
+        char newline ='\n';
+        char singlequete ='\'';
+        char doubleQuote ='\"';
+        System.out.println("ini contoh " + tab + "literal tab");
+        System.out.println("ini contoh " + newline + "literal newline");
+        System.out.println("ini contoh " + singlequete + "literal singlequete");
+        System.out.println("ini contoh " + doubleQuote + "literal doubleQuote");
+
+    }
+}
