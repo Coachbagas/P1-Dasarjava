b382b50dadf7643d78dc2c558fd02687f196707f
bagas ardianto <bagasardiantobks2020@gmail.com>
2022-03-14 19:21:13 +0700

    mencetak literal string

---
+++ src/M_mencetakliteralString.java
@@ -0,0 +1,6 @@
+public class M_mencetakliteralString {
+    public static void main(String[] args) {
+        String welcome ="Pemograman berorientasi objek dengan java";
+        System.out.println(welcome);
+    }
+}
