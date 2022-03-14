58551d04d1955bec051405ac2e9128879b2a7ba9
bagas ardianto <bagasardiantobks2020@gmail.com>
2022-03-14 19:38:53 +0700

    contoh statment

---
+++ src/P_Statment.java
@@ -0,0 +1,10 @@
+public class P_Statment {
+    public static void main(String[] args) {
+        System.out.println("semoga saya bisa belajar");
+        System.out.println("pemograman berorientasi objek");
+        int angka =10;
+        char charA='a';
+        System.out.println(angka);
+        System.out.println(charA);
+    }
+}
